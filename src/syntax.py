aed55efc8a3b6f2949cad2b0a01240c547527533
Animesh Dutta <dutta.animesh08@gmail.com>
2018-07-01 16:20:18 +0530

    Added keywords and comments for Java and C

--- src/syntax.py
+++ src/syntax.py
@@ -11,6 +11,16 @@ syntax = {
                      'as', 'def', 'from', 'nonlocal', 'while',
                      'assert', 'del', 'global', 'not', 'with',
                      'async', 'elif', 'if', 'or', 'yield'],
-        "comment": '#'
-    }
-}
+        "comment": '#' },
+    "java":{
+        "keywords":['if','else','break','continue','while','do',
+            'for','double','long','int','float','String','System','print','println',
+            'boolean','byte','switch','case','catch','throw','throws','import','extends'
+            'class','public','return','try','short','super','this','char'],
+        "comment": '/'},
+    "c":{
+        "keywords":['if','else','break','continue','while','do',
+            'for','double','long','int','float','sizeof','void','struct','union',
+            'auto','switch','void','case','register','char','signed'],
+        "comment":'//' }
+         }
